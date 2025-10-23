95ad675138ae2eb584cbd88c75ab7787d3fb84ff
michael <michaelmackenzie3101@gmail.com>
2025-10-23 17:58:26 +0300

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
+import React from "react";
+import BotPage from "./pages/BotPage";
 
 function App() {
-
   return (
-    <>
-    
-      
-    </>
-  )
+    <div className="min-h-screen bg-gray-950 text-white flex flex-col items-center justify-center">
+      <BotsPage />
+    </div>
+  );
 }
 
-export default App
+export default App;
